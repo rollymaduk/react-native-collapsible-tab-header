792cb07dc12aa0990bd21bd843a666c171fff01f
ronald <rolly.maduk@gmail.com>
2017-10-04 03:19:22 +0100

    fix tests

--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,17 +1,17 @@
 import test from 'ava';
-import {getHeight} from './helpers';
+import { getHeight } from './helpers';
 import chai from 'chai';
 
 const should = chai.should();
 
 test('helpers.getHeight - when given percent string returns ratio of number', (t) => {
-  helpers.getHeight('50%', 250).should.eql(125);
+  getHeight('50%', 250).should.eql(125);
 });
 
 test('helpers.getHeight - percent string without base returns percent as number', (t) => {
-  helpers.getHeight('50%').should.eql(50);
+  getHeight('50%').should.eql(50);
 });
 
 test('helpers.getHeight - number returns number', (t) => {
-  helpers.getHeight(500).should.eql(500);
+  getHeight(500).should.eql(500);
 });
